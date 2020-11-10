9b38ba5bd426d3b808fdf4219ed8409c491baf7f
Iris Wilson <iriswilsonr@gmail.com>
2020-11-09 19:22:20 -0600

    Update conexion.php

--- global/conexion.php
+++ global/conexion.php
@@ -6,7 +6,7 @@ private function __construct (){}
  
 public static function conectar(){
 $pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-self::$conexion= new PDO('mysql:host=174.136.30.134;dbname=nammagic_main','nammagic_user_main','W1ls0nis9',$pdo_options);
+self::$conexion= new PDO('mysql:host=localhost;dbname=nammagic_main','nammagic_user_main','W1ls0nis9',$pdo_options);
 return self::$conexion;
 }
 }
