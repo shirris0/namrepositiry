5e7a7709879d5decece45f35b748c998e582b9d5
Iris Wilson <iriswilsonr@gmail.com>
2020-11-09 19:37:13 -0600

    Update administrar_libro.php

--- global/administrar_libro.php
+++ global/administrar_libro.php
@@ -19,7 +19,7 @@ $libro= new Libro();
 		
 
 		// Check if image file is a actual image or fake image
-			if(isset($_POST["submit"])) {
+		/*	if(isset($_POST["submit"])) {
 				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 				if($check !== false) {
 				echo "File is an image - " . $check["mime"] . ".";
@@ -61,6 +61,7 @@ $libro= new Libro();
 				echo "Sorry, there was an error uploading your file.";
 				}
 			}
+			*/
 		
 		$libro->setNombre($_POST['nombre']);
 		//$libro->setNombre($path);
