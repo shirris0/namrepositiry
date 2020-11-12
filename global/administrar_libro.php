08a70535e94c2100f2417cd8261a0adf0b83ccf3
Iris Wilson <iriswilsonr@gmail.com>
2020-11-12 00:39:28 -0600

    Update administrar_libro.php

--- global/administrar_libro.php
+++ global/administrar_libro.php
@@ -22,36 +22,36 @@ $libro= new Libro();
 			if(isset($_POST["submit"])) {
 				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 				if($check !== false) {
-				echo "File is an image - " . $check["mime"] . ".";
+				#echo "File is an image - " . $check["mime"] . ".";
 				$uploadOk = 1;
 				} else {
-				echo "File is not an image.";
+				#echo "File is not an image.";
 				$uploadOk = 0;
 				}
 			}
 			
 			// Check if file already exists
 			if (file_exists($target_file)) {
-				echo "Sorry, file already exists.";
+				#echo "Sorry, file already exists.";
 				$uploadOk = 0;
 			}
 			
 			// Check file size
 			if ($_FILES["fileToUpload"]["size"] > 500000) {
-				echo "Sorry, your file is too large.";
+				#echo "Sorry, your file is too large.";
 				$uploadOk = 0;
 			}
 			
 			// Allow certain file formats
 			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 			&& $imageFileType != "gif" && $imageFileType != "mp3" && $imageFileType != "mp4" && $imageFileType != "wav" && $imageFileType != "avi") {
-				echo "Sorry, lo siento este tipo de archivos no esta permitido.";
+				#echo "Sorry, lo siento este tipo de archivos no esta permitido.";
 				$uploadOk = 0;
 			}
 			
 			// Check if $uploadOk is set to 0 by an error
 			if ($uploadOk == 0) {
-				echo "Sorry, your file was not uploaded.";
+				#echo "Sorry, your file was not uploaded.";
 			// if everything is ok, try to upload file
 			} else {
 				try {
@@ -65,7 +65,7 @@ $libro= new Libro();
 					
 				//echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
 				} else {
-				echo "Sorry, there was an error uploading your file.";
+				#echo "Sorry, there was an error uploading your file.";
 				header('Location: https://defensoresdelanaturaleza.mx/');
 				}
 			}
