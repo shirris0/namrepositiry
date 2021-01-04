c247bea0fdba7b98d9f8e8192a83d79a3c8a683d
minoro94 <leyva393@hotmail.com>
2021-01-04 03:05:20 -0700

    Inclusion de los Rompecabezas del Libro 1

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,219 +1,253 @@
 var host = "/namrepository/";
 
-$( document ).ready(function() {
+$(document).ready(function() {
 
-    
 
-    showBook = function(libro,cuento){
-        var url = host+"libros/"+libro+"/Cuento"+cuento+"/cuento"+cuento+".html";
-        var bookName = "cuento"+cuento;
-        $("#cuentoContainer").load(url, function(){
+
+    showBook = function(libro, cuento) {
+        var url = host + "libros/" + libro + "/Cuento" + cuento + "/cuento" + cuento + ".html";
+        var bookName = "cuento" + cuento;
+        $("#cuentoContainer").load(url, function() {
             var elemento = document.getElementById(bookName);
             var myBook = $(elemento).turn({
-                display:'double',
+                display: 'double',
                 acceleration: true,
                 elevation: 50,
                 autoCenter: true,
-                duration : 1300,
-                turnCorners:'tl,tr'
+                duration: 1300,
+                turnCorners: 'tl,tr'
             });
             var audio = document.getElementById("audio");
             audio.play();
-           
-            setTimeout(function(){ 
+
+            setTimeout(function() {
                 myBook.turn("next");
-                if(bookName  == "cuento1"){
-                    setTimeout(function(){ 
+                if (bookName == "cuento1") {
+                    setTimeout(function() {
                         myBook.turn("next")
                     }, 1000);
                 }
             }, 1500);
 
-            $("#"+bookName).bind("turned", function(event, page, view) {  
+            $("#" + bookName).bind("turned", function(event, page, view) {
 
             });
-            
-            
+
+
         });
-    
-        
-       
+
+
+
     };
 
-    descargarIlustraciones = function(ilustracion){
-        var url = host+"libros/"+ilustracion+"/Ilustraciones.zip";
+    descargarIlustraciones = function(ilustracion) {
+        var url = host + "libros/" + ilustracion + "/Ilustraciones.zip";
         //window.open(url);
         var req = new XMLHttpRequest();
         req.open("GET", url, true);
         req.responseType = "blob";
-      
-        req.onload = function (event) {
-          var blob = req.response;
-          console.log(blob.size);
-          var link=document.createElement('a');
-          link.href=window.URL.createObjectURL(blob);
-          link.download="Ilustracion_"+ ilustracion+ ".zip";
-          link.click();
+
+        req.onload = function(event) {
+            var blob = req.response;
+            console.log(blob.size);
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = "Ilustracion_" + ilustracion + ".zip";
+            link.click();
         };
-      
+
         req.send();
     }
-   
 
-    descargarLibro = function(libro){
-        var url = host+"libros/"+libro+"/libro.pdf";
+
+    descargarLibro = function(libro) {
+        var url = host + "libros/" + libro + "/libro.pdf";
         //window.open(url);
         var req = new XMLHttpRequest();
         req.open("GET", url, true);
         req.responseType = "blob";
-      
-        req.onload = function (event) {
-          var blob = req.response;
-          console.log(blob.size);
-          var link=document.createElement('a');
-          link.href=window.URL.createObjectURL(blob);
-          link.download="Libro_"+ libro+ ".pdf";
-          link.click();
+
+        req.onload = function(event) {
+            var blob = req.response;
+            console.log(blob.size);
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = "Libro_" + libro + ".pdf";
+            link.click();
         };
-      
+
         req.send();
     }
 
-    descargarAudio = function(url,name){
+    descargarAudio = function(url, name) {
         //window.open(url);
         var req = new XMLHttpRequest();
         req.open("GET", url, true);
         req.responseType = "blob";
-      
-        req.onload = function (event) {
-          var blob = req.response;
-          console.log(blob.size);
-          var link=document.createElement('a');
-          link.href=window.URL.createObjectURL(blob);
-          link.download=name+ ".mp3";
-          link.click();
+
+        req.onload = function(event) {
+            var blob = req.response;
+            console.log(blob.size);
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = name + ".mp3";
+            link.click();
         };
-      
+
         req.send();
     }
 
 
 
 
-    cargarCarousel = function(){
+    cargarCarousel = function() {
         // Testimonials carousel (uses the Owl Carousel library)
         $(".testimonials-carousel").owlCarousel({
             autoplay: true,
             dots: true,
             loop: true,
             responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 1
-            },
-            900: {
-                items: 2
-            }
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                900: {
+                    items: 2
+                }
             }
         });
     }
 
-    pruebaFunction = function(html,idMenu,funcion){
+    pruebaFunction = function(html, idMenu, funcion) {
         $("#menuBar li").removeClass("active");
         $("#hero").remove();
-        $("#main").load(host+"aboutContent.html",function(){
+        $("#main").load(host + "aboutContent.html", function() {
             cargarCarousel();
         });
-        $("#about").addClass("active");     
-        $('.back-to-top').trigger("click");   
+        $("#about").addClass("active");
+        $('.back-to-top').trigger("click");
         $('.mobile-nav-toggle').trigger("click");
     }
 
-   
-    leerManual = function(manual){
-        var url = host+"manuales/"+manual+".pdf";
+
+    leerManual = function(manual) {
+        var url = host + "manuales/" + manual + ".pdf";
         window.open(url);
     }
 
-    hoverLibroMenu = function(element,libro) {
-        var url = host+"assets/img/libro"+libro+"Hover.png";
-        element.setAttribute('src', url);
-      }
-      
-    unhoverLibroMenu = function(element,libro) {
-        var url = host+"assets/img/libro"+libro+".png";
+    hoverLibroMenu = function(element, libro) {
+        var url = host + "assets/img/libro" + libro + "Hover.png";
         element.setAttribute('src', url);
     }
 
-    hoverLibroMenuTerritorio = function(element,libro) {
-        var url = host+"assets/img/"+libro+"Hover.png";
+    unhoverLibroMenu = function(element, libro) {
+        var url = host + "assets/img/libro" + libro + ".png";
         element.setAttribute('src', url);
-      }
-      
-    unhoverLibroMenuTerritorio = function(element,libro) {
-        var url = host+"assets/img/"+libro+".png";
+    }
+
+    hoverLibroMenuTerritorio = function(element, libro) {
+        var url = host + "assets/img/" + libro + "Hover.png";
+        element.setAttribute('src', url);
+    }
+
+    unhoverLibroMenuTerritorio = function(element, libro) {
+        var url = host + "assets/img/" + libro + ".png";
         element.setAttribute('src', url);
     }
 
 
-    abrirCapacitacion = function(id){
+    abrirCapacitacion = function(id) {
 
-        switch(id){
+        switch (id) {
             case 1:
                 $("#videoCap").attr('src', 'https://www.youtube.com/embed/VMKcnkwNAUE');
                 $("#tituloCap").text("Proceso Nacional de Reconciliación con la Naturaleza (PNRN) - Introducción");
                 $("#subtituloCap").text("");
-                
-            break;
-            
+
+                break;
+
             case 2:
                 $("#videoCap").attr('src', 'https://www.youtube.com/embed/GDCR_sgEeiw');
                 $("#tituloCap").text("Colección de Libros “Cuentos desde el Ombligo de la Luna” - Libro 1");
                 $("#subtituloCap").text("Consumismo");
-            break;
+                break;
 
             case 3:
                 $("#videoCap").attr('src', 'https://www.youtube.com/embed/LWhMpCKlzb0');
                 $("#tituloCap").text("Colección de Libros “Cuentos desde el Ombligo de la Luna” - Libro 2");
                 $("#subtituloCap").text("Empaques y etiquetados");
-            break;
+                break;
 
             case 4:
                 $("#videoCap").attr('src', 'https://www.youtube.com/embed/sFQH_4vAwoo');
                 $("#tituloCap").text("Colección de Libros “Cuentos desde el Ombligo de la Luna” - Libro 3");
                 $("#subtituloCap").text("Huella del Bienestar");
-            break;
+                break;
 
             case 5:
                 $("#videoCap").attr('src', 'https://www.youtube.com/embed/XMncB8U2nok');
                 $("#tituloCap").text("Colección de Libros “Cuentos desde el Ombligo de la Luna” - Libro 4");
                 $("#subtituloCap").text("Enfermedades de la tierra");
-            break;
+                break;
 
             case 6:
                 $("#videoCap").attr('src', 'https://www.youtube.com/embed/Wr9gUVPNWlw');
                 $("#tituloCap").text("Colección de Libros “Cuentos desde el Ombligo de la Luna” - Libro 5");
                 $("#subtituloCap").text("Enfermedades de la sociedad");
-            break;
+                break;
 
             case 7:
                 $("#videoCap").attr('src', 'https://www.youtube.com/embed/FqFUUC3SGck');
                 $("#tituloCap").text("Colección de Libros “Cuentos desde el Ombligo de la Luna” - Libro 6");
                 $("#subtituloCap").text("La reconciliación");
-            break;
-          
-              
+                break;
+
+
         }
 
-        
-        
+
+
     }
 
-    
+    load = function() {
+        var padre = document.getElementById("padre");
+    };
+
+    mostrarRompecabezas = (libro, cuento) => {
+        var hijo = document.getElementById("hijo");
+        padre.removeChild(hijo);
+        var elemento = document.createElement('div');
+        if (libro == 1) {
+            if (cuento == 1) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=0e10dd67197b&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 2) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=11bb7bc177bf&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 3) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=395461425f0d&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 4) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=361640945f6d&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>')
+            } else if (cuento == 5) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=3db59ccc6090&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 6) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=1d0102dfef5a&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 7) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=1a002325016e&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 8) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=1ac8a9516158&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 9) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=15ba229321eb&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 10) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=3a9c214ceddf&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            } else if (cuento == 11) {
+                elemento.innerHTML = ('<iframe src="https://www.jigsawplanet.com/?rc=play&amp;pid=1ae085bab033&amp;view=iframe" style="width:100%;height:600px" frameborder=0 allowfullscreen></iframe>');
+            }
+        }
+        padre.appendChild(elemento);
+    };
 
 
 
-   
+
 });
