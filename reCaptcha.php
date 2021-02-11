6e9188d41979da715bbc7c692da3e59bf3593cd3
minoro94 <leyva393@hotmail.com>
2021-02-10 23:16:39 -0700

    Modificando reCaptcha-php

--- reCaptcha.php
+++ reCaptcha.php
@@ -6,7 +6,7 @@ function validarToken($token){
     $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".SECRETE_KEY."&response={$google_token}"."&remoteip=".$_SERVER['REMOTE_ADDR']);
     $response = json_decode($response);
     $response = (array) $response;
-    echo print_r($response);
+    
     if($response['success'] && ($response['score']) && $response['score'] > 0.5){
         return true;
     }else{
