5d6ed206c16a0344d4d33daa7552ea7a965607e2
minoro94 <leyva393@hotmail.com>
2021-02-25 14:39:36 -0700

    Arreglando error del contador de infografias

--- assets/js/capacitacionController.js
+++ assets/js/capacitacionController.js
@@ -1,79 +1,79 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-    cargarInfografias(null,1);
+    cargarInfografias(null, 1);
 
 
 
 });
 
 var noLibro1 = 12;
-var noLibro2 = 13;
+var noLibro2 = 12;
 var noLibro3 = 12;
 var noLibro4 = 12;
 var noLibro5 = 12;
 var noLibro6 = 12;
 
-cargarInfografias = function(element,libro){
+cargarInfografias = function(element, libro) {
     //Limpieza
     $("#idCarouselInfografias").empty();
     $("#idOLInfografias").empty();
-    
+
 
     var noInfografias;
 
-    switch(libro){
-        case 1: 
+    switch (libro) {
+        case 1:
             noInfografias = noLibro1;
-        break;
+            break;
         case 2:
             noInfografias = noLibro2;
-        break;
+            break;
         case 3:
             noInfografias = noLibro3;
-        break;
+            break;
         case 4:
             noInfografias = noLibro4;
-        break;
+            break;
         case 5:
             noInfografias = noLibro5;
-        break;
+            break;
         case 6:
             noInfografias = noLibro6;
-        break;
+            break;
     }
 
     var cont = 1;
     var str = "";
     var str2 = "";
-    while (cont <= noInfografias){
+    while (cont <= noInfografias) {
 
-        if(cont == 1 ){
-            str =  str.concat("<div class='carousel-item active'><img class='d-block w-100' src='assets/img/capacitacion/libro"+libro+"/"+cont+".jpg' alt='First slide'></div>");
-            str2 = str2.concat("<li data-target='#carousel-thumb' data-slide-to='"+(cont-1)+"' class='active'> <img class='d-block w-100' src='assets/img/capacitacion/libro"+libro+"/"+cont+".jpg' class='img-fluid'></li>");
-        }else{
-            str =  str.concat("<div class='carousel-item'><img class='d-block w-100' src='assets/img/capacitacion/libro"+libro+"/"+cont+".jpg' alt='Second slide'></div>");
-            str2 = str2.concat("<li data-target='#carousel-thumb' data-slide-to='"+(cont-1)+"'><img class='d-block w-100' src='assets/img/capacitacion/libro"+libro+"/"+cont+".jpg' class='img-fluid'></li>");
+        if (cont == 1) {
+            str = str.concat("<div class='carousel-item active'><img class='d-block w-100' src='assets/img/capacitacion/libro" + libro + "/" + cont + ".jpg' alt='First slide'></div>");
+            str2 = str2.concat("<li data-target='#carousel-thumb' data-slide-to='" + (cont - 1) + "' class='active'> <img class='d-block w-100' src='assets/img/capacitacion/libro" + libro + "/" + cont + ".jpg' class='img-fluid'></li>");
+        } else {
+            str = str.concat("<div class='carousel-item'><img class='d-block w-100' src='assets/img/capacitacion/libro" + libro + "/" + cont + ".jpg' alt='Second slide'></div>");
+            str2 = str2.concat("<li data-target='#carousel-thumb' data-slide-to='" + (cont - 1) + "'><img class='d-block w-100' src='assets/img/capacitacion/libro" + libro + "/" + cont + ".jpg' class='img-fluid'></li>");
         }
-    
+
         cont++;
-        
+
     }
 
     $("#idCarouselInfografias").append(str);
     $("#idOLInfografias").append(str2);
-    
+
 
 }
 
 
-hoverLibroInfografia = function(element,libro) {
-    var url = host+"assets/img/capacitacion/libro"+libro+"Hover.png";
+hoverLibroInfografia = function(element, libro) {
+    var url = host + "assets/img/capacitacion/libro" + libro + "Hover.png";
     element.setAttribute('src', url);
-    element.setAttribute('style','width: 100%');
-  }
-  
-unhoverLibroInfografia = function(element,libro) {
-    var url = host+"assets/img/capacitacion/libro"+libro+".png";
+    element.setAttribute('style', 'width: 100%');
+}
+
+unhoverLibroInfografia = function(element, libro) {
+    var url = host + "assets/img/capacitacion/libro" + libro + ".png";
     element.setAttribute('src', url);
-    element.setAttribute('style','width: 60%');
+    element.setAttribute('style', 'width: 60%');
 }
