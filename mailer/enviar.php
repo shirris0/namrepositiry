1ee5559645d1763793badf16ae9c613fc7e329f5
minoro94 <leyva393@hotmail.com>
2021-03-22 09:15:54 -0700

    Actualizando pass correo

--- mailer/enviar.php
+++ mailer/enviar.php
@@ -32,7 +32,7 @@ if(!validarToken($_POST['token-google'])){
         //$smtpHost="smtp.gmail.com";
         $smtpUsuario = "info@defensoresdelanaturaleza.mx";  // Mi cuenta de correo
         //$smtpUsuario = "rleyvacastro@gmail.com";
-        $smtpClave = '$jLhVcBPPDOa';  
+        $smtpClave = 'tyrant-003';  
         //$smtpClave = "tyrant-003";
         $mail = new PHPMailer();
         $mail->IsSMTP();
